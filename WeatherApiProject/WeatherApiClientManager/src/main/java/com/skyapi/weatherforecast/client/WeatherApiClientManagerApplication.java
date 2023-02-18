f98ad343faedda72a61b0496a80ca38c6a69bbb3
rohit1039 <rohitparida0599@gmail.com>
2023-02-18 08:38:42 +0530

    first commit

---
+++ WeatherApiProject/WeatherApiClientManager/src/main/java/com/skyapi/weatherforecast/client/WeatherApiClientManagerApplication.java
@@ -0,0 +1,13 @@
+package com.skyapi.weatherforecast.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherApiClientManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherApiClientManagerApplication.class, args);
+	}
+
+}
