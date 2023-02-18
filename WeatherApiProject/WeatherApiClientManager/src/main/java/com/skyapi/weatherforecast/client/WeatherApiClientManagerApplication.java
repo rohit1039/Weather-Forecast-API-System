2ce70fef95bf61778ad44882a67c19a1f280fccf
rohit1039 <rohitparida0599@gmail.com>
2023-02-18 11:54:26 +0530

    first commit

--- WeatherApiProject/WeatherApiClientManager/src/main/java/com/skyapi/weatherforecast/client/WeatherApiClientManagerApplication.java
+++ WeatherApiProject/WeatherApiClientManager/src/main/java/com/skyapi/weatherforecast/client/WeatherApiClientManagerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WeatherApiClientManagerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WeatherApiClientManagerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherApiClientManagerApplication.class, args);
+    }
 
 }
--- WeatherApiProject/WeatherApiService/src/main/java/com/skyapi/weatherforecast/WeatherApiServiceApplication.java
+++ WeatherApiProject/WeatherApiService/src/main/java/com/skyapi/weatherforecast/WeatherApiServiceApplication.java
@@ -1,13 +1,13 @@
 package com.skyapi.weatherforecast;
 
-import org.springframework.boot.SpringApplication; 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WeatherApiServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WeatherApiServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherApiServiceApplication.class, args);
+    }
 
 }
