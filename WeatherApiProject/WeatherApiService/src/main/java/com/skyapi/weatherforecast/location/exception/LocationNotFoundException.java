575772e04083ee0b7d8c1ce76f33674215eec4c7
rohit1039 <rohitparida0599@gmail.com>
2023-02-19 21:20:26 +0530

    Updated 🎉

--- WeatherApiProject/WeatherApiService/src/main/java/com/skyapi/weatherforecast/location/exception/LocationNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.skyapi.weatherforecast.location.exception;
-
-public class LocationNotFoundException extends RuntimeException {
-
-    public LocationNotFoundException(String message) {
-
-        super(message);
-    }
-}
