3d1d18ec0c400db746e031d225319d793403bf42
rohit1039 <rohitparida0599@gmail.com>
2023-02-23 07:22:17 +0530

    Code updated

--- WeatherApiProject/WeatherApiService/src/main/java/com/skyapi/weatherforecast/WeatherApiServiceApplication.java
+++ WeatherApiProject/WeatherApiService/src/main/java/com/skyapi/weatherforecast/WeatherApiServiceApplication.java
@@ -1,12 +1,20 @@
 package com.skyapi.weatherforecast;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class WeatherApiServiceApplication {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(WeatherApiServiceApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(WeatherApiServiceApplication.class, args);
+
+        ApplicationContext applicationContext = SpringApplication.run(WeatherApiServiceApplication.class, args);
+
+        LOGGER.info("Application currently running: {}", applicationContext.getId());
     }
 }
